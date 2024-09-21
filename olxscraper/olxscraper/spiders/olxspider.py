fc83adb76b85f97bdf9ada2bed5ba68c57aec305
Sreejith Jayaprakash <sreejithjayaprakash194@gmail.com>
2024-09-22 01:54:49 +0530

    scraped some data

--- olxscraper/olxscraper/spiders/olxspider.py
+++ olxscraper/olxscraper/spiders/olxspider.py
@@ -3,21 +3,38 @@ import scrapy
 
 class OlxspiderSpider(scrapy.Spider):
     name = "olxspider"
-    allowed_domains = ["www.olx.in"]
-    start_urls = ["https://www.olx.in/"]
-
+    allowed_domains = ['www.olx.in']
+    start_urls = ['https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723']
+    
     def parse(self, response):
-        # Extracting all the listings
-        listings = response.css('li._1DNjI')
+        # Extracting the property URLs on the page
+        property_links = response.css('li a::attr(href)').extract()
+        for link in property_links:
+            if link:
+                full_url = response.urljoin(link)
+                yield scrapy.Request(url=full_url, callback=self.parse_property)
+        
+        # Handling dynamic loading (e.g., load more)
+        load_more_button = response.css('button[data-aut-id="btnLoadMore"]::attr(href)').get()
+        if load_more_button:
+            yield scrapy.Request(url=response.urljoin(load_more_button), callback=self.parse)
 
-        # Iterating through each listing
-        for listing in listings:
-            # Extracting the itemTitle
-            item_title = listing.css('span[data-aut-id="itemTitle"]::text').get()
-    
-            # Extracting the itemPrice
-            item_price = listing.css('span[data-aut-id="itemPrice"]::text').get()
-    
-            # Printing the result
-            print(f"Title: {item_title}, Price: {item_price}")
+    def parse_property(self, response):
+        # Extract the necessary details from each property
+        yield {
+            "property_name": response.css('h1[data-aut-id="itemTitle"]::text').get(),
+            "property_id": response.url.split("-iid-")[-1],
+            "breadcrumbs": response.css('ol li a::text').extract(),
+            "price": {
+                "amount": response.css('span[data-aut-id="itemPrice"]::text').re_first(r'\d+'),
+                "currency": "₹"
+            },
+            "image_url": response.css('figure img::attr(src)').get(),
+            "description": response.css('div[data-aut-id="itemDescriptionContent"] p::text').get(),
+            "seller_name": response.css('span[data-aut-id="profileName"]::text').get(),
+            "location": response.css('div[data-aut-id="itemLocation"] span::text').get(),
+            "property_type": "Apartments",  # Assuming it's apartments for now
+            "bathrooms": response.css('span[data-aut-id="itemBathrooms"]::text').get(),
+            "bedrooms": response.css('span[data-aut-id="itemBedrooms"]::text').get()
+        }
 
